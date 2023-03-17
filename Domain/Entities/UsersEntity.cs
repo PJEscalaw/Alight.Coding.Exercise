c0d9d1fbb1e08f4a35fa242f553937fadc3c9e5a
pjescalaw <jm.obias@mobixsystemsinc.com>
2023-03-17 23:46:09 +0800

    Added entities for database.

---
+++ Domain/Entities/UsersEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+/// <summary>
+/// Entity for Users.
+/// </summary>
+public class UsersEntity
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } //MANDATORY
+    public string LastName { get; set; } //MANDATORY
+    public string Email { get; set; } //MANDATORY, UNIQUE
+    public int AddressId { get; set; }
+    public IEnumerable<int> EmploymentIds { get; set; }
+}
