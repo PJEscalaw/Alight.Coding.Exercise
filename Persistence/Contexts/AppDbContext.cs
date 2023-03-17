98381fa3d8200b8908a8e955df4a297dbda6093d
pjescalaw <jm.obias@mobixsystemsinc.com>
2023-03-18 00:04:04 +0800

    Added db contexts.

---
+++ Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts;
+
+/// <summary>
+/// DbContext of the app.
+/// </summary>
+public class AppDbContext : DbContext
+{
+    public DbSet<EmploymentsEntity> Employments { get; set; }
+    public DbSet<AddressesEntity> Addresses { get; set; }
+    public DbSet<UsersEntity> Users { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+}
