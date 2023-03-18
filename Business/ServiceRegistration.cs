e90b2cc69a14996dac6546bd05d86372dfd90510
pjescalaw <jm.obias@mobixsystemsinc.com>
2023-03-18 10:40:10 +0800

    Added pipelines.

---
+++ Business/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Business.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System.Reflection;
+
+namespace Business;
+
+public static class ServiceRegistration
+{
+    public static void AddBusiness(this IServiceCollection services)
+    {
+        _ = services.AddMediatR(Assembly.GetExecutingAssembly());
+        _ = services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceLoggingBehavior<,>));
+        _ = services.AddSingleton(Log.Logger);
+    }
+}
